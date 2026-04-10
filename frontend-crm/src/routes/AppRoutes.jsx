4b089dad506e83f3849965e4d782d2aa9bc984aa
Phuc <phuca6k5@gmail.com>
2026-04-10 13:23:11 +0700

    feat(frontend): [FCW0-16] [FCW0-17] implement customer order total and create order api

--- frontend-crm/src/routes/AppRoutes.jsx
+++ frontend-crm/src/routes/AppRoutes.jsx
@@ -1,22 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
-// IMPORT ĐÚNG PATH
-import Login from "../pages/Login";
-// import Dashboard from "../pages/Dashboard";
-// import Products from "../pages/Products";
-// import Customers from "../pages/Customers";
-// import CustomerDetail from "../pages/CustomerDetail";
 import MainLayout from "../layouts/MainLayout";
+import Customers from "../pages/Customers";
+import Login from "../pages/Login";
 import RequireAuth from "./RequireAuth";
 
 export default function AppRoutes() {
     return (
         <BrowserRouter>
             <Routes>
-                {/* Login riêng */}
                 <Route path="/login" element={<Login />} />
 
-                {/* Layout */}
                 <Route
                     element={(
                         <RequireAuth>
@@ -24,10 +18,8 @@ export default function AppRoutes() {
                         </RequireAuth>
                     )}
                 >
-                    {/*<Route path="/" element={<Dashboard />} />*/}
-                    {/*<Route path="/products" element={<Products />} />*/}
-                    {/*<Route path="/customers" element={<Customers />} />*/}
-                    {/*<Route path="/customers/:id" element={<CustomerDetail />} />*/}
+                    <Route path="/" element={<Navigate to="/customers" replace />} />
+                    <Route path="/customers" element={<Customers />} />
                 </Route>
             </Routes>
         </BrowserRouter>
